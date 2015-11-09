32e572225cf35a70c404a454357ff131194796c1
RedstoneAlmeida <mineps3marcos@gmail.com>
2015-11-09 11:10:59 -0200

    Create Logout.php

---
+++ src/ServerAuth/Commands/Logout.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * ServerAuth (v2.11) by EvolSoft
+ * Developer: EvolSoft (Flavius12)
+ * Website: http://www.evolsoft.tk
+ * Date: 31/08/2015 10:56 AM (UTC)
+ * Copyright & License: (C) 2015 EvolSoft
+ * Licensed under MIT (https://github.com/EvolSoft/ServerAuth/blob/master/LICENSE)
+ */
+ 
+namespace ServerAuth\Commands;
+
+use pocketmine\permission\Permission;
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+
+use pocketmine\utils\TextFormat;
+
+use ServerAuth\ServerAuth;
+
+class Logout implements CommandExecutor {
+
+	public function __construct(ServerAuth $plugin){
+        $this->plugin = $plugin;
+    }
+    
+public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+    	$fcmd = strtolower($cmd->getName());
+    	switch($fcmd){
+    		case "logout":
+    			if($sender->hasPermission("serverauth.logout")){
+    				//Player Sender
+    				if($sender instanceof Player){
+    					$cfg = $this->plugin->getConfig()->getAll();
+    					//Check if logout is enabled
+    					if($cfg["logout"]["enabled"]){
+    						$status = ServerAuth::getAPI()->deauthenticatePlayer($sender);
+    						if($status == ServerAuth::SUCCESS){
+    							$sender->sendMessage($this->plugin->translateColors("&", $cfg["prefix"] . ServerAuth::getAPI()->getConfigLanguage()->getAll()["logout"]["logout-success"]));
+    						}elseif($status == ServerAuth::ERR_USER_NOT_AUTHENTICATED){
+    							$sender->sendMessage($this->plugin->translateColors("&", $cfg["prefix"] . ServerAuth::getAPI()->getConfigLanguage()->getAll()["errors"]["user-not-authenticated"]));
+    						}elseif($status == ServerAuth::ERR_USER_NOT_REGISTERED){
+    							$sender->sendMessage($this->plugin->translateColors("&", $cfg["prefix"] . ServerAuth::getAPI()->getConfigLanguage()->getAll()["errors"]["user-not-registered"]));
+    						}elseif($status == ServerAuth::CANCELLED){
+    								$sender->sendMessage($this->plugin->translateColors("&", $cfg["prefix"] . ServerAuth::getAPI()->getConfigLanguage()->getAll()["operation-cancelled"]));
+    						}else{
+    							$sender->sendMessage($this->plugin->translateColors("&", $cfg["prefix"] . ServerAuth::getAPI()->getConfigLanguage()->getAll()["errors"]["generic"]));
+    						}
+    					}else{
+    						$sender->sendMessage($this->plugin->translateColors("&", $cfg["prefix"] . ServerAuth::getAPI()->getConfigLanguage()->getAll()["logout"]["disabled"]));
+    					}
+    					break;
+    				}else{ //Console Sender
+    					$sender->sendMessage($this->plugin->translateColors("&", ServerAuth::PREFIX . "&cYou can only perform this command as a player"));
+    					break;
+    				}
+    			}else{
+    				$sender->sendMessage($this->plugin->translateColors("&", "&cYou don't have permissions to use this command"));
+    				break;
+    			}
+    			return true;
+    			}
+    	}
+}
+?>
